fb6faf82349c782e89218bd5dd1efa4e2e36f5c3
leandro <leandroadames12345@gmail.com>
2023-04-07 11:08:10 +0900

    complete

--- index.js
+++ index.js
@@ -1,5 +1,58 @@
-(function() {
-  'use strict';
+let stopButton = document.getElementById("stopButton");
+let stopLight = document.getElementById("stopLight");
+let slowButton = document.getElementById("slowButton");
+let slowLight = document.getElementById("slowLight");
+let goButton = document.getElementById("goButton");
+let goLight = document.getElementById("goLight");
+let count = 0;
 
-  // YOUR CODE HERE
-})();
+
+stopButton.addEventListener("click", function (){
+stopLight.classList.toggle("stop");
+ })
+
+slowButton.addEventListener("click", function (){
+  slowLight.classList.toggle("slow");
+ })
+
+goButton.addEventListener("click", function (){
+  goLight.classList.toggle("go");
+
+ })
+
+ // mouse movent listeners
+
+ function handlemouseEntered(event){
+  console.log(`entered ${event.target.textcontent} button`);
+ }
+
+ function handlemouseLeft(event) {
+  console.log(`left ${event.target.textcontent} button`)
+ }
+
+stopButton.addEventListener('mouseenter', handlemouseEntered);
+slowButton.addEventListener('mouseenter', handlemouseEntered);
+goButton.addEventListener('mouseenter', handlemouseEntered);
+
+stopButton.addEventListener('mouseenter', handlemouseLeft);
+slowButton.addEventListener('mouseenter', handlemouseLeft);
+goButton.addEventListener('mouseenter', handlemouseLeft);
+
+
+
+
+ function handleBulbOn(event) {
+  console.log(`${event.target.textcontent} bulb on`)
+
+}
+goButton.addEventListener('click', handleBulbOn);
+
+
+//  function handlemouseEntered(event){
+//   console.log(`entered ${event.target.textcontent} button`);
+
+// (function() {
+//   'use strict';
+
+//   // YOUR CODE HERE
+// })();
